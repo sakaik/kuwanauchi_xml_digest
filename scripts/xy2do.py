1542d1629741c339d578a74588e083d0e97e3e62
sakaik <sakaik@gmail.com>
2023-02-18 14:58:01 +0900

    xy2do:remove using numpy

--- scripts/xy2do.py
+++ scripts/xy2do.py
@@ -10,13 +10,9 @@ copyright @sakaik, 2023
 '''
 ##おことわり:　上記利用規約は適宜変更していくことがあります
 
+import math
 
-import numpy as np
-
-#TODO math化/rad-deg変換はnpを使っているがここだけなら自前実装してもよさげ。（本スクリプトを動かすための前提条件をなるべく減らしたい）
-
-# xyから度へ
-
+### xy to Degree function ###
 # xy2do: args
 #   平面直角座標系から緯度経度へ。
 #   x, y: 平面直角座標系における X,Yの値
@@ -39,7 +35,7 @@ def xy2do(x, y, japaneseKei):
                (44.0, 140+(15.0/60)), (44.0, 142+(15.0/60)), (44.0, 144+(15.0/60)), (26.0, 142.0        ), (26.0, 127.5        ),
                (26.0, 124.0        ), (26.0, 131.0,       ), (20.0, 136.0        ), (26.0, 154.0        )
                ]
-        return np.deg2rad(ary[kei])
+        return (math.radians(ary[kei][0]), math.radians(ary[kei][1]))
     
     def makeAryA(n):
         A0 = 1 + (n**2)/4.0 + (n**4)/64.0
@@ -48,8 +44,7 @@ def xy2do(x, y, japaneseKei):
         A3 = -(35.0/48)*(n**3 - (5.0/16)*(n**5))
         A4 = (315.0/512)*(n**4)
         A5 = -(693.0/1280)*(n**5)
-
-        return np.array([A0, A1, A2, A3, A4, A5])
+        return [A0, A1, A2, A3, A4, A5]
 
     def makeAryBeta(n):
         beta1 = (1.0/2)*n - (2.0/3)*(n**2) + (37.0/96)*(n**3) - (1.0/360)*(n**4) - (81.0/512)*(n**5)
@@ -57,7 +52,7 @@ def xy2do(x, y, japaneseKei):
         beta3 = (17.0/480)*(n**3) - (37.0/840)*(n**4) - (209.0/4480)*(n**5)
         beta4 = (4397.0/161280)*(n**4) - (11.0/504)*(n**5)
         beta5 = (4583.0/161280)*(n**5)
-        return np.array([np.nan, beta1, beta2, beta3, beta4, beta5])
+        return [math.nan, beta1, beta2, beta3, beta4, beta5]
 
     def makeAryDelta(n):
         delta1 = 2*n - (2.0/3)*(n**2) - 2*(n**3) + (116.0/45)*(n**4) + (26.0/45)*(n**5) - (2854.0/675)*(n**6)
@@ -66,30 +61,30 @@ def xy2do(x, y, japaneseKei):
         delta4 = (4279.0/630)*(n**4) - (332.0/35)*(n**5) - (399572.0/14175)*(n**6)
         delta5 = (4174.0/315)*(n**5) - (144838.0/6237)*(n**6)
         delta6 = (601676.0/22275)*(n**6)
-        return np.array([np.nan, delta1, delta2, delta3, delta4, delta5, delta6])
+        return [math.nan, delta1, delta2, delta3, delta4, delta5, delta6]
 
     def calcPartOfSbar_phi0(aryA, phi0):
         res = 0.0
         for j in range(1, 5+1):
-            res += aryA[j] * np.sin(2*j*phi0)
+            res += aryA[j] * math.sin(2*j*phi0)
         return res
 
     def calcPartOfXidash(aryBeta, xi, eta):
         res = 0.0
         for j in range(1, 5+1):
-            res += aryBeta[j]*np.sin(2*j*xi)*np.cosh(2*j*eta)
+            res += aryBeta[j]*math.sin(2*j*xi)*math.cosh(2*j*eta)
         return res
 
     def calcPartOfEtadash(aryBeta, xi, eta):
         res = 0.0
         for j in range(1, 5+1):
-            res += aryBeta[j]*np.cos(2*j*xi)*np.sinh(2*j*eta)
+            res += aryBeta[j]*math.cos(2*j*xi)*math.sinh(2*j*eta)
         return res
 
     def calcPartOfPhi(aryDelta, chi):
         res = 0.0
         for j in range(1, 6+1):
-            res += aryDelta[j]*np.sin(2*j*chi)
+            res += aryDelta[j]*math.sin(2*j*chi)
         return res
 
 
@@ -119,14 +114,14 @@ def xy2do(x, y, japaneseKei):
     etadash = eta - calcPartOfEtadash(aryBeta, xi, eta)
 
     # χ
-    chi = np.arcsin( np.sin(xidash)/np.cosh(etadash))
+    chi = math.asin( math.sin(xidash)/math.cosh(etadash))
 
     # 経度緯度！ φ, λ
     phi = chi + calcPartOfPhi(aryDelta, chi)
-    lambda_ = lambda0 + np.arctan(np.sinh(etadash)/np.cos(xidash))
+    lambda_ = lambda0 + math.atan(math.sinh(etadash)/math.cos(xidash))
 
-    latitude = np.rad2deg(phi)
-    longitude = np.rad2deg(lambda_)
+    latitude = math.degrees(phi)
+    longitude = math.degrees(lambda_)
 
     return (latitude, longitude)
 
@@ -140,4 +135,5 @@ if __name__ == '__main__':
     x = -15035.6705
     y = 17617.1597
     (lat, lon) = xy2do(x,y,9)
-    print(x, y,' to ',lat, lon)
+    print(x, y, ' is ', 35.864320, 140.028410, "(Expect)")
+    print(x, y,' to ',lat, lon, "Calced")
